a644f9885183e41c6ae0b65304d1afa602662cc3
Nandi03 <nchavda260@gmail.com>
2022-03-07 18:00:55 +0000

    Attempt at plotting graphs for logTransactions

--- Prototype/src/exp/graphs.py
+++ Prototype/src/exp/graphs.py
@@ -13,24 +13,28 @@ times = []
 
 # EXAMPLE : Test 1 for logTransactions: execution time when the transactions logged are in random order.
 def log_transactions_test_1(num):
-    time_taken = timeit.repeat(stp.logTransaction(tdg.generateTransactionData(1)), repeat = num, number = 1000)
 
+    startTime = timeit.default_timer()
+    for i in range(num):
+        stp.logTransaction(tdg.generateTransactionData(1))
+    endTime = timeit.default_timer()
+
+    time_taken = endTime - startTime
     # adding data for the graph
     insertions.append(num)
     times.append(time_taken)
     return time_taken
-
 
 def test_graph():
     # y = np.array(times)
     # x = np.array(insertions)
 
     # example data
-    y = np.array([1, 1, 1.1, 1.15, 1.17])
-    x = np.array([1, 10, 100, 1000, 10000])
+    y = np.array([1, 2])
+    x = np.array([1, 2])
     # plt.xscale("log")
 
-    plt.plot(x, y, '-', color = 'red')
+    plt.plot(x, y, '-', color='red')
 
     plt.ylabel('Average log time')
     plt.xlabel('Number of insertions')
@@ -41,3 +45,20 @@ def test_graph():
 
 
 test_graph()
+
+def plotLogTransactions(x, times):
+    y = []
+    numOfTransactions = len(x)
+    for i in range(numOfTransactions):
+        y.append(times[i][1])
+    y = np.array(y)
+    x = np.array(x)
+
+    plt.plot(x, y, '-', color = "red")
+
+    plt.ylabel('Average log time')
+    plt.xlabel('Number of insertions')
+
+    # setting y-axis range
+    plt.ylim([0.5, 1.5])
+    plt.show()

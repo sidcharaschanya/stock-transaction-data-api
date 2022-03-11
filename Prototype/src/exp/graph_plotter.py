f20c81cd6c07ad56d22c6a43c141a759c0bfa7eb
Sid Charaschanya <sidcharaschanya@gmail.com>
2022-03-11 17:29:42 +0000

    Next level prettier than the prettiest graphical plots :)

--- Prototype/src/exp/graph_plotter.py
+++ Prototype/src/exp/graph_plotter.py
@@ -11,11 +11,13 @@ class GraphPlotter:
         self.__x = self.__ef.get_n_transactions_list()
 
     @staticmethod
-    def __get_bounds(y: list) -> t.List[float]:
-        y.sort()
-        return [0.2 * y[int(len(y) * 0.03)], y[int(len(y) * 0.97)] * 1.2]
+    def __get_y_lim(ys: t.List[list]) -> t.List[float]:
+        return [
+            min([sorted(y)[int((len(y) - 1) * 0.03)] * 0.75 for y in ys]),
+            max([sorted(y)[int((len(y) - 1) * 0.97)] * 1.1 for y in ys])
+        ]
 
-    def __general_plot(self, title: str, ys: list, colors: list, funcs: list, labels: list = None) -> None:
+    def __general_plot(self, op: str, ys: list, colors: list, funcs: list, labels: list = None) -> None:
         for i, (y, color, func) in enumerate(zip(ys, colors, funcs)):
             if labels:
                 plt.plot(self.__x, y, color + ".", label = labels[i])
@@ -24,54 +26,69 @@ class GraphPlotter:
 
             plt.plot(self.__x, np.poly1d(np.polyfit(func(self.__x), y, 1))(func(self.__x)), color + "--")
 
-        plt.title(title)
-        plt.xlabel("Number of Transactions Under a Particular Stock")
-        plt.ylabel("Time of Execution (s)")
-        plt.ylim(self.__get_bounds(ys[0]))
-
         if labels:
             plt.legend()
 
+        plt.title(op + " Time Vs. Transaction Count")
+        plt.xlabel("Number of Transactions Under a Particular Stock")
+        plt.ylabel("Execution Time (s)")
+        plt.ylim(self.__get_y_lim(ys))
         plt.show()
 
-    def plot_graphs(self):
+    def plot_graphs(self) -> None:
         self.__general_plot(
-            "Log Transactions",
+            "Log Transaction",
             [self.__ef.get_times(Case.LOG_RANDOM), self.__ef.get_times(Case.LOG_SORTED)],
-            ["b", "m"],
+            ["m", "b"],
             [np.log2, np.log2],
-            ["Random", "Sorted"]
+            ["Random Insertion Order", "Sorted Insertion Order"]
         )
 
         self.__general_plot(
-            "Sorted List Retrieval",
+            "Sorted Transactions",
             [self.__ef.get_times(Case.SORTED)],
-            ["c"],
+            ["b"],
             [lambda x: x]
         )
 
         self.__general_plot(
             "Min Transactions",
             [self.__ef.get_times(Case.MIN)],
+            ["r"],
+            [np.log2]
+        )
+
+        self.__general_plot(
+            "Max Transactions",
+            [self.__ef.get_times(Case.MAX)],
             ["g"],
             [np.log2]
         )
 
-        # self.__general_plot('Max Transactions',
-        #                     self.__ef.get_times(Case.MAX), best_func = lambda x: log2(x))
-        # self.__general_plot('Retrieve Floor',
-        #                     self.__ef.get_times(Case.FLOOR_RANDOM),
-        #                     self.__ef.get_times(Case.FLOOR_EXISTING),
-        #                     'Random', 'Existing', lambda x: log2(x))
-        # self.__general_plot('Retrieve Ceiling',
-        #                     self.__ef.get_times(Case.CEILING_RANDOM),
-        #                     self.__ef.get_times(Case.CEILING_EXISTING),
-        #                     'Random', 'Existing', lambda x: log2(x))
-        # self.__general_plot('Retrieve Range',
-        #                     self.__ef.get_times(Case.RANGE_ALL),
-        #                     self.__ef.get_times(Case.RANGE_RANDOM),
-        #                     'Full range', 'Random Range')
+        self.__general_plot(
+            "Floor Transactions",
+            [self.__ef.get_times(Case.FLOOR_RANDOM), self.__ef.get_times(Case.FLOOR_EXISTING)],
+            ["r", "b"],
+            [np.log2, np.log2],
+            ["Random Threshold Values", "Threshold Values Existing in Tree"]
+        )
+
+        self.__general_plot(
+            "Ceiling Transactions",
+            [self.__ef.get_times(Case.CEILING_RANDOM), self.__ef.get_times(Case.CEILING_EXISTING)],
+            ["g", "b"],
+            [np.log2, np.log2],
+            ["Random Threshold Values", "Threshold Values Existing in Tree"]
+        )
+
+        self.__general_plot(
+            "Range Transactions",
+            [self.__ef.get_times(Case.RANGE_RANDOM), self.__ef.get_times(Case.RANGE_ALL)],
+            ["c", "b"],
+            [lambda x: x, lambda x: x],
+            ["Random Range", "Full Range"]
+        )
 
 
-plotter = GraphPlotter(10000, 100, 10)
+plotter = GraphPlotter(10000, 100, 20)
 plotter.plot_graphs()
